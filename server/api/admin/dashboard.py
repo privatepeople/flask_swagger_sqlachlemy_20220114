0f7770aa5963218ef9d251c33a485c904558cbc9
이승훈 <negrat@naver.com>
2022-01-20 11:37:21 +0900

    성별에 따른 count 구해보기
    - query (모델) 이 있으므로, filter()는 생략해도, 어느 테이블인지 인식 가능

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -36,16 +36,21 @@ class AdminDashboard(Resource):
         amount_list = [{'lecture_title': row[0], 'amount': int(row[1])} for row in lecture_fee_amount]
 
             
-        # 모든 남성 유저 목록 출력
-        male_users = Users.query.filter(Users.is_male == True).all()
+        # 남성 회원수 / 여성 회원수
+        gender_user_count_list = db.session.query(Users.is_male, db.func.count(Users.id))\
+            .group_by(Users.is_male)\
+            .all()
         
-        print(male_users)
+        gender_user_counts = [ {'is_male': row[0], 'user_count': int(row[1])} for row in gender_user_count_list ]
             
+        print(gender_user_count_list)
+
         return {
             'code': 200,
             'message': '관리자용 각종 통계 api',
             'data': {
                 'live_user_count': users_count,
                 'lecture_amount': amount_list, # 각 강의 별 총합
+                'gender_user_counts': gender_user_counts,
             }
         }
